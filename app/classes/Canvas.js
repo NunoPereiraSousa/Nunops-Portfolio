9bd45a8bc74d3719661e0030e2513f5d39ddde64
Nuno Pereira Sousa <9180579@esmad.ipp.pt>
2021-08-03 12:02:21 +0100

    Finished

--- app/classes/Canvas.js
+++ app/classes/Canvas.js
@@ -365,15 +365,10 @@ export default class Canvas {
 
       void main(){
         vec2 newUV = vUv;
-        // float area = smoothstep(0.3,0.,vUv.y);
-        // area = pow(area, 4.);
-        // newUV.x -= (vUv.x - 0.5)*0.03*area*scrollSpeed;
         float area = smoothstep(0.25,0.,vUv.y);
         newUV.x -= ((vUv.x + area * 0.5) - 1.) *0.085*area;
 
         gl_FragColor = texture2D( tDiffuse, newUV);
-        
-        // gl_FragColor = vec4(area,0.,0.,1.);
       }
       `
     };
--- app/shaders/fragment.glsl
+++ app/shaders/fragment.glsl
@@ -4,15 +4,10 @@ uniform sampler2D uImage;
 uniform float time;
 
 void main()	{
-
 	vec2 newUV = vUv;
-
 
 	vec4 oceanView = texture2D(uImage, newUV);
 
-
-	// gl_FragColor = vec4(vNoise,0.,0.,1.);
 	gl_FragColor = oceanView;
 	// gl_FragColor.rgb += 0.01*vec3(vNoise);
-	// gl_FragColor = vec4(vNoise,0.,0.,1.);
 }
